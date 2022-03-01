26679a1306b04007d922726532388ea33905ae07
hbkwong <>
2022-02-28 19:08:00 -0800

    Initial data.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,12 @@
-const App = () => {
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+
+const App = (props) => {
+  useEffect(() => {
+    props.dispatch(handleInitialData());
+  }, []);
   return <div>Starter Code</div>;
 };
 
-export default App;
+export default connect()(App);
