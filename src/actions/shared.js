3659bd0c5fdda5771effd1434ef3ba4594e736f1
Lamees91 <lamees.osama.91@gmail.com>
2023-02-27 19:25:20 +0200

    add code till step 8

---
+++ src/actions/shared.js
@@ -0,0 +1,16 @@
+import { getInitialData } from "../utils/api";
+import { receiveUsers } from "./users";
+import { receiveTweets } from "./tweets";
+import { setAuthedUser } from "./authedUser";
+
+const AUTHED_ID = "tylermcginnis";
+
+export function handleInitialData() {
+  return (dispatch) => {
+    return getInitialData().then(({ users, tweets }) => {
+      dispatch(receiveUsers(users));
+      dispatch(receiveTweets(tweets));
+      dispatch(setAuthedUser(AUTHED_ID));
+    });
+  };
+}
---
+++ src/middleware/index.js
@@ -0,0 +1,6 @@
+import logger from "./logger";
+
+import thunk from "redux-thunk";
+import { applyMiddleware } from "redux";
+
+export default applyMiddleware(thunk, logger);
