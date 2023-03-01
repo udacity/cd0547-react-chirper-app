ca44769dc26475d24075dac3197d2fbe910485c4
Lamees91 <lamees.osama.91@gmail.com>
2023-03-01 18:35:33 +0200

    Add loading bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import { useEffect } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
+import LoadingBar from "react-redux-loading-bar";
 
 const App = (props) => {
   useEffect(() => {
     props.dispatch(handleInitialData());
   }, []);
-  return <div>Starter Code</div>;
+
+  return (
+    <div>
+      <LoadingBar />
+      {props.loading === true ? null : <Dashboard />}
+    </div>
+  );
 };
 
-export default connect()(App);
+const mapStateToProps = ({ authedUser }) => ({
+  loading: authedUser === null,
+});
+
+export default connect(mapStateToProps)(App);
